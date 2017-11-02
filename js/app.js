43151856519837d8c7aa4306768df2e4a4ee4c28
MelB <Melusimaphumulo@gmail.com>
2017-11-02 13:25:35 +0200

    dashboard for stylist

--- js/app.js
+++ js/app.js
@@ -1,7 +1,18 @@
 //Declaring app
-var Imakapp = angular.module('Imakapp');
+var app = angular.module('app',['ngRoute']);
 
-//Controller
-Imakapp.controller('booking_controller',['$scope',function(){
-    $scope.head = "Working Fine";
+//Routing login page as main page and register as second
+app.config(['$routeProvider', function($routeProvider){
+        $routeProvider.when('/',{
+            templateUrl: 'views/user_login.html',
+            controller: 'user_login_controller'
+        }).when('/RegisterView',{
+            templateUrl: 'views/user_register.html',
+            controller: 'user_register_controller'
+        }).when('/HomeView',{
+            templateUrl: 'views/home.html',
+            controller: 'home_controller'
+        }).otherwise({
+            redirectTo: '/'
+        });
 }]);
