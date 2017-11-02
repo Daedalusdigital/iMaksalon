15cf74a9f9eebc7f36486942f591f7a932b7fb1c
MelB <Melusimaphumulo@gmail.com>
2017-11-02 13:51:01 +0200

    fixing controllers

--- js/app.js
+++ js/app.js
@@ -1,23 +1,5 @@
 //Declaring app
-var app = angular.module('app',['ngRoute']);
-
-//Routing login page as main page and register as second
-app.config(['$routeProvider', function($routeProvider){
-        $routeProvider.when('/',{
-            templateUrl: 'views/user_login.html',
-            controller: 'user_login_controller'
-        }).when('/RegisterView',{
-            templateUrl: 'views/user_register.html',
-            controller: 'user_register_controller'
-        }).when('/HomeView',{
-            templateUrl: 'views/home.html',
-            controller: 'home_controller'
-        }).otherwise({
-            redirectTo: '/'
-        });
-
-
-}]);
+var app = angular.module('Imakapp',['']);
 
 app.controller("emailController",['$scope','SubscribeService', function($scope,SubscribeService){
     
