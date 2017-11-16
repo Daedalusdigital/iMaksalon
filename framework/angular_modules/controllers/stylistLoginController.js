c8e108ff59e1bf4b558f17066e848dfcd23c36b5
MelB <Melusimaphumulo@gmail.com>
2017-11-16 03:42:59 +0200

    approve and decline btn works :)

--- framework/angular_modules/controllers/stylistLoginController.js
+++ framework/angular_modules/controllers/stylistLoginController.js
@@ -28,16 +28,41 @@ app.controller("testingController", ['$scope','loginService','SessionService',fu
 }]);
 app.controller('viewingController',['$scope','$http','loginService','SessionService', function($scope,$http,loginService,SessionService){
      var stylistIds = SessionService.get("stylestID");
-     var obj = angular.toJson({stylist_id: stylistIds});
-     
-$http.post("https://prod-29.southcentralus.logic.azure.com:443/workflows/2aeb751e26da433db142414354443c7a/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=ggufLib2UUpQ2NRQ1GU6RopnvdFOPa0k4SicG-HTYJs",obj)
-        .then(function(response){
-            //$scope.bookings = response.data.records;
-          $scope.bookinglist = response.data;
-          console.log(response.data);
-        },function(error){
-            console.log(error);
-        });
+
+     $scope.loadContent = function(){
+        var obj = angular.toJson({stylist_id: stylistIds});
+        
+       $http.post("https://prod-29.southcentralus.logic.azure.com:443/workflows/2aeb751e26da433db142414354443c7a/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=ggufLib2UUpQ2NRQ1GU6RopnvdFOPa0k4SicG-HTYJs",obj)
+           .then(function(response){
+               //$scope.bookings = response.data.records;
+             $scope.bookinglist = response.data;
+             console.log(response.data);
+           },function(error){
+               console.log(error);
+           });
+     }
+
+        $scope.alterBooking = function(_id,btn_name,clientId,stylistId,booked_ser){
+            var obj = angular.toJson({        id: _id,
+                                              btnName: btn_name,
+                                              booked_service: booked_ser,
+                                              client_id:clientId,
+                                              stylist_id : stylistId
+                                      });
+                                     
+          $http.post("https://prod-18.southcentralus.logic.azure.com:443/workflows/e6a5f9eca53e4eb085d9765bd8bdc995/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=VMSIYNHfpk3uCm7wrEr4jRtubaNDxCBc5Z0q4TL5A54",obj)
+                  .then(function(response){
+                      //$scope.bookings = response.data.records;
+                    console.log( response.data);
+                  },function(error){
+                      console.log(error);
+                  });
+                  $scope.loadContent();
+                  $('#preloader').show();
+                  setTimeout(function(){ $('#preloader').fadeOut('slow') }, 2000);
+                  
+                 
+        }
 }]);
 
 app.factory('loginService',['$http',function($http){
