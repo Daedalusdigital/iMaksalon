a8222fe9354f72b91481c2571b67a8710159ab9b
welcome <welcomeking@outlook.com>
2017-11-17 13:50:54 +0200

    push
    Push

--- js/pricelist.js
+++ js/pricelist.js
@@ -1,7 +1,7 @@
 var app = angular.module("csvApp", ["ngSanitize", "ngCsv"]);
 
 app.controller("csvController",function($scope, $http){
-  $http.get('https://prod-19.southcentralus.logic.azure.com:443/workflows/03520c6f053b40f3b917e3695cdcdcf8/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=9Lm3IX8yB9vMpyCA2p2I2fKJcr5behrTCOecT4w8Rdw').
+  $http.get('https://prod-28.southcentralus.logic.azure.com:443/workflows/471cd7b33694476dbbaded25185a8e26/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=Tn-Tyq6LUvoT4lu9Oe9qFNvibkx7acDHSFlw0sXJ5Zo').
     success(function(data) {
       $scope.data1 = data;
       $scope.random = function() {
