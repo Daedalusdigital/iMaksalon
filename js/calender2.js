c7ab655b14e7286e2fe095b7ef703cd6a3967621
MelB <Melusimaphumulo@gmail.com>
2017-11-30 12:20:01 +0200

    type ahead not finish

--- js/calender2.js
+++ js/calender2.js
@@ -65,7 +65,18 @@ app.controller('reportCtl',['$scope','$http',function($scope,$http){
         $scope.clientid = clientId;
         $scope.clientname = clientName;
         $scope.clientsname = clientSname;
-        
+
+        function MainController($scope, $http) {
+            
+            $scope.people = [];
+                    $scope.recent_posts = [];
+            $http.get('https://prod-14.southcentralus.logic.azure.com:443/workflows/ddfbff4cd859434897cf388edee47955/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=k5pJbHXL04UUSW4tqaiB38pGbI195PuLkqQ81yVeUoo',$scope.people).success(function(data)
+            {
+                    $scope.people = data;
+            });   
+            
+        };
+
         $scope.makeBooking = function(values){
             //alert("Clicked");
             var serviceDate = $scope.newDateSelected;
