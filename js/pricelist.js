bb811b1b79d710d50afccc02ffae394571658b8e
spha123 <sphamandlanhlanhlafortunedube@gmail.com>
2017-12-13 12:32:02 +0200

    Refresher

--- js/pricelist.js
+++ js/pricelist.js
@@ -1,7 +1,6 @@
 var app = angular.module("csvApp", ["ngSanitize", "ngCsv"]);
 
 app.controller("productListController",function($scope, $http){
-<<<<<<< HEAD
 
 
   $scope.refreshProductList = function(){
@@ -9,10 +8,6 @@ app.controller("productListController",function($scope, $http){
     $scope.loading = true;
 
     $http.get('https://prod-28.southcentralus.logic.azure.com:443/workflows/471cd7b33694476dbbaded25185a8e26/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=Tn-Tyq6LUvoT4lu9Oe9qFNvibkx7acDHSFlw0sXJ5Zo').
-=======
-  
-  $http.get('https://prod-28.southcentralus.logic.azure.com:443/workflows/471cd7b33694476dbbaded25185a8e26/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=Tn-Tyq6LUvoT4lu9Oe9qFNvibkx7acDHSFlw0sXJ5Zo').
->>>>>>> 8ec827ac2e389cf2c73a422c374184327ca6b4e5
     success(function(data) {
       $scope.data1 = data;
       $scope.loading = false;
