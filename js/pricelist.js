c5b779ef0e4444310c7262772bfc24913cf3827c
MelB <Melusimaphumulo@gmail.com>
2017-12-13 12:51:23 +0200

    loader for products done

--- js/pricelist.js
+++ js/pricelist.js
@@ -26,8 +26,14 @@ app.controller("productListController",function($scope, $http){
   
 });
 
+
 app.controller("serviceListController",function($scope, $http){
   
+  
+$scope.refreshServiceList = function(){
+  
+  $scope.loading = true;
+      
   $http.get('https://prod-19.southcentralus.logic.azure.com:443/workflows/03520c6f053b40f3b917e3695cdcdcf8/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=9Lm3IX8yB9vMpyCA2p2I2fKJcr5behrTCOecT4w8Rdw').
     success(function(data) {
       $scope.data1 = data;
@@ -35,8 +41,12 @@ app.controller("serviceListController",function($scope, $http){
       $scope.random = function() {
         return 0.5 - Math.random();
       }
+      $scope.loading = false;
     }).
     error(function(data) {
       // log error
+      $scope.loading = false;
     });
+
+  }
 });
